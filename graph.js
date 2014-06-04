df6df696c9016735e562d46347da64dbb6b01537
Yakov Tchenak <ytchenak@gmail.com>
2014-06-04 23:37:49 +0300

    dfs

--- graph.js
+++ graph.js
@@ -2,9 +2,9 @@
 
 //JSON.stringify(g)
 
-// process.on('uncaughtException', function(err) {
-//     console.log(err);
-// });
+process.on('uncaughtException', function(err) {
+    console.log(err);
+});
 
 function Vertex (data) {
 	this.data = data;
@@ -13,94 +13,94 @@ Vertex.prototype.toString = function() {
 		return this.data;
 };
 
+//--------------------------------------------
+
 function Edge (source, target) {
 	this.source = source;
 	this.target = target;
-	this.toString = function() {
-		return this.source + '=>' + this.target;
-	}
 }
 
+Edge.prototype.toString = function() {
+		return this.source + '=>' + this.target;
+};
+
+
+//unidirected graph
 function Graph () {
-	this.edges1 = {};
-	this.edges2 = {};
+	this.edges = {};
+}
 
-	this.addVertex = function(v) {
-		if( !(this.edges1[v]) ) 
-			this.edges1[v1] = [];
-		if( !(this.edges2[v]) ) 
-			this.edges2[v2] = [];
-	}
+Graph.prototype.addVertex = function(v) {
+	if( !(this.edges[v]) ) 
+		this.edges[v1] = [];
+}
 
-	this.addEdge = function(v1, v2) {
-		if( !(this.edges1[v1]) ) 
-			this.edges1[v1] = [];
-		this.edges1[v1].push( new Edge(v1, v2));
-	}
+Graph.prototype.addVertex = function(v) {
+	if( !(this.edges[v]) ) 
+		this.edges[v1] = [];
+}
 
-	this.toString = function() {
-		var s = '\n';
-		for( var v in this.edges1) {
-			s += v;
-			s += ' => [ ';
-			this.edges1[v].forEach( function(e) {
-				s += e.target + ' ';
-			});
-			s += '] ';
-		} 
-		return s;
-	}
+Graph.prototype.addEdge = function(v1, v2) {
+	if( !(this.edges[v1]) ) 
+		this.edges[v1] = [];
+	this.edges[v1].push( new Edge(v1, v2));
+}
 
-	this.dfs = function(v) {
-		var marked  = {};
-		var stack  = [];
-		this.dfsRecursive(v, marked);
-		// this.dfsStack(v, marked, stack);
-	}
-	this.dfsRecursive = function(v, marked) {
-		var _this = this;
-		marked[v] = true;
+Graph.prototype.toString = function() {
+	var s = '';
+	for( var v in this.edges) {
+		s += v;
+		s += ' => [' + this.edges[v].map( function(e) {
+			return e.target.data;
+		});
+		s += ']; ';
+	} 
+	return s;
+}
 
-		console.log('visiting Vertex ' + v);
-	
-		var vEdges = this.edges1[v];
-		if( vEdges == undefined) {
-			return;
-		}
+Graph.prototype.dfs = function(v, result) {
+	var discovered = {} //hash
+	var result = [];
+	this.dfsRecursive(v, discovered, result);
+	return result;
+}
+Graph.prototype.dfsRecursive = function(v, discovered, result) {
+	var _this = this;
+	discovered[v] = true;
+
+	var vEdges = this.edges[v];
+	if( vEdges != undefined) {
 
 		vEdges.forEach( function(e) {
 			var vTarget = e.target;
-			if( marked[vTarget] ) {
-				console.log('Vertex ' + vTarget + ' is already visited');
-				return;
+			if( !discovered[vTarget] ) {
+				_this.dfsRecursive(vTarget, discovered, result);
 			}
-			_this.dfsRecursive(vTarget, marked);
 		});
 	}
-	this.dfsStack = function(v, marked, stack) {
-		var _this = this;
-		stack.pop(v);
-	
-		while ( stack.length > 0) {
-			v = stack.shift(); 
-			marked[v] = true;
-			console.log('visiting Vertex ' + v);
-			var vEdges = this.edges1[v];
-			if( vEdges == undefined) {
-				return;
-			}
+	result.unshift(v) //reverse post order
+}
+Graph.prototype.bfs = function(v) {
+	var _this = this;
+	var discovered = {}; //hash
+	var queue = [];
+	queue.push(v);
 
-			vEdges.forEach( function(e) {
-				var vTarget = e.target;
-				if( !marked[vTarget] ) {
-					stack.push(vTarget);
-				}
-			});
+	while ( queue.length > 0) {
+		v = queue.shift(); 
+		if( !discovered[v]) {
+			console.log('visiting Vertex ' + v);
+			discovered[v] = true;
+			var vEdges = this.edges[v];
+			if( vEdges != undefined) {
+				vEdges.forEach( function(e) {
+					var vTarget = e.target;
+					queue.push(vTarget);
+				});
+			}
 		}
 	}
-
 }
-
 
 
 var v1 = new Vertex('1');
@@ -117,11 +117,14 @@ g.addEdge(v1, v3);
 g.addEdge(v2, v4);
 g.addEdge(v2, v5);
 g.addEdge(v3, v6);
-//g.addEdge(v6, v1);
+g.addEdge(v5, v3);
+g.addEdge(v3, v4);
 
-
-console.log('graph:' + g.toString()); 
-g.dfs(v1);
+console.log('graph: ' + g.toString()); 
+var result = g.dfs(v1);
+console.log('topological sorted: '+ result.map( function(v) {
+	return v.data;
+}));
 
 
 
